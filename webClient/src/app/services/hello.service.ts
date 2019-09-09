1849278e6f26087e099e4c160d64c83fad4a0943
Usman Haider <usmanhaider89@gmail.com>
2019-09-09 20:10:48 +0500

    Using code from angular-app.

--- webClient/src/app/services/hello.service.ts
+++ webClient/src/app/services/hello.service.ts
@@ -1,15 +1,4 @@
 
-/*
-  This program and the accompanying materials are
-  made available under the terms of the Eclipse Public License v2.0 which accompanies
-  this distribution, and is available at https://www.eclipse.org/legal/epl-v20.html
-  
-  SPDX-License-Identifier: EPL-2.0
-  
-  Copyright Contributors to the Zowe Project.
-*/
-export { AppModule as pluginModule } from './app/app.module';
-export { AppComponent as pluginComponent } from './app/app.component';
 
 /*
   This program and the accompanying materials are
@@ -20,3 +9,39 @@ export { AppComponent as pluginComponent } from './app/app.component';
   
   Copyright Contributors to the Zowe Project.
 */
+
+import { Injectable } from '@angular/core';
+import { Http} from '@angular/http';
+import { Observable } from 'rxjs/Observable';
+
+@Injectable()
+export class HelloService {
+  private destination:string;
+  
+  constructor(private http: Http){}
+
+  setDestination(path: string):void {
+    this.destination = path;
+  }
+
+  sayHello(text: string): Observable<any> {
+    const requestBody = {
+      "_objectType": "org.zowe.zlux.sample.angular.request.hello",
+      "_metaDataVersion": "1.0.0",
+      "messageFromClient": text
+    }
+    return this.http.post(this.destination, requestBody);
+  }
+
+}
+
+/*
+  This program and the accompanying materials are
+  made available under the terms of the Eclipse Public License v2.0 which accompanies
+  this distribution, and is available at https://www.eclipse.org/legal/epl-v20.html
+  
+  SPDX-License-Identifier: EPL-2.0
+  
+  Copyright Contributors to the Zowe Project.
+*/
+
